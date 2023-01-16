cdcf0aebd6bcecf678fddeeded47e38c43a05f8c
Oliver <75665514+Oliver-was-taken@users.noreply.github.com>
2023-01-16 10:30:18 +0100

    improve rate-function  ¬State: 🟢

--- src/main/java/ch/bbw/m411/connect4/Connect4ArenaMain.java
+++ src/main/java/ch/bbw/m411/connect4/Connect4ArenaMain.java
@@ -140,7 +140,7 @@ public class Connect4ArenaMain {
     public interface Connect4Player {
 
         /**
-         * Called before the game starts and guaranteed to only be called once per livetime of the player.
+         * Called before the game starts and guaranteed to only be called once per lifetime of the player.
          *
          * @param board       the starting board, usually an empty board.
          * @param colorToPlay the color of this player
@@ -149,7 +149,7 @@ public class Connect4ArenaMain {
 
         /**
          * Perform a next move, will only be called if the Game is not over yet.
-         * Each player has to keep an internal state of the 4x7 board, wher the 0-index is on the bottom row.
+         * Each player has to keep an internal state of the 4x7 board, where the 0-index is on the bottom row.
          * The index-layout looks as:
          * <pre>
          * 30 31 32 33 34 35 36
